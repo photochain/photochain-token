8c9e0e919c663e1a89c3556444e9654a54072d45
Jakub Stefanski <code@jakubstefanski.com>
2018-04-07 12:24:36 +0200

    Improve migrations contract

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,23 +1,20 @@
-pragma solidity ^0.4.17;
+pragma solidity 0.4.21;
 
-contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
+import { Ownable } from "./Ownable.sol";
 
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
 
-  function Migrations() public {
-    owner = msg.sender;
-  }
+contract Migrations is Ownable {
+    /* solhint-disable var-name-mixedcase */
+    /* solhint-disable func-param-name-mixedcase */
 
-  function setCompleted(uint completed) public restricted {
-    last_completed_migration = completed;
-  }
+    uint public last_completed_migration;
 
-  function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
-  }
+    function setCompleted(uint completed) public onlyOwner {
+        last_completed_migration = completed;
+    }
+
+    function upgrade(address new_address) public onlyOwner {
+        Migrations upgraded = Migrations(new_address);
+        upgraded.setCompleted(last_completed_migration);
+    }
 }
