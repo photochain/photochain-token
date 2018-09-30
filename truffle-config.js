4f0d8cd039a7d9370be26f004d5f099e3dd19fcc
Jakub <jakub@jksf.co>
2018-09-30 14:45:10 +0200

    Deploy to mainnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,11 +1,18 @@
 module.exports = {
     networks: {
+        mainnet: {
+            from: '0xED3f6E6B5fFc52E37Ea6a02D78193b558EF0C428',
+            host: 'localhost',
+            port: 8545,
+            network_id: '1',
+            gasPrice: 10350000000 // 10.35 GWei
+        },
         rinkeby: {
             from: '0xED3f6E6B5fFc52E37Ea6a02D78193b558EF0C428',
             host: 'localhost',
             port: 8545,
             network_id: '4',
-            gasPrice: 14350000000 // 14.35 GWei
+            gasPrice: 10350000000 // 10.35 GWei
         }
     }
 };
